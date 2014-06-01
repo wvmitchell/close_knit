ef0ab2b2d5a353a6feb0d6dcd8cb3c8c8383c9c6
Will Mitchell <wvmitchell@gmail.com>
2014-06-01 02:06:37 -0600

    added functionality for finding resturants

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'vcr'
 require 'shoulda/matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -43,4 +44,49 @@ RSpec.configure do |config|
 
   # Include factory girl methods
   config.include FactoryGirl::Syntax::Methods
+
+  config.around(:each, :vcr) do |example|
+    name = example.metadata[:full_description].split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_")
+    options = {}
+    options[:record] = example.metadata[:record] if example.metadata[:record]
+    VCR.use_cassette(name, options) { example.call }
+  end
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir        = 'spec/vcr'
+  c.hook_into                   :webmock
+
+  c.default_cassette_options    = {
+    :record                     => :new_episodes,
+    :decode_compressed_response => true,
+    # Because psych is binary encoding response headers marked with ASCII-8BIT
+    # https://groups.google.com/forum/?fromgroups#!topic/vcr-ruby/2sKrJa86ktU
+    # And syck's output is much easier to read
+    :serialize_with             => :psych,
+  }
+
+  # Pretty print your json so it's not all on one line
+  # From discussion here: https://github.com/myronmarston/vcr/pull/147
+  # https://gist.github.com/26edfe7669cc7b85e164
+  c.before_record do |i|
+    type = Array(i.response.headers['Content-Type']).join(',').split(';').first
+    code = i.response.status.code
+
+    if type =~ /[\/+]json$/ or 'text/javascript' == type
+      begin
+        data = JSON.parse i.response.body
+      rescue
+        if code != 404
+          puts
+          warn "VCR: JSON parse error for Content-type #{type}"
+          warn "Your unparseable json is: " + i.response.body.inspect
+          puts
+        end
+      else
+        i.response.body = JSON.pretty_generate data
+        i.response.update_content_length_header
+      end
+    end
+  end
 end
