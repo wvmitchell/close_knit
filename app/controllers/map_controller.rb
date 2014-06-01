b9512f2194d55f64ba27665d121be623ae2d91d2
Will Mitchell <wvmitchell@gmail.com>
2014-05-31 22:13:54 -0600

    added controller action and route

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,8 @@
 class MapController < ApplicationController
   def index
   end
+
+  def get_scores
+    render text: {capitol_hill: {crime: 89, recreation: 78, social: 89}}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :map
 
   root to: 'map#index'
+
+  get 'scores' => 'map#get_scores'
 end
